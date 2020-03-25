a4ba1b77df6315dd1fac58911bbeddd69487e41d
Jakkrit Tultirat (59160639) <50378545+benzkikyo@users.noreply.github.com>
2020-03-25 09:33:40 +0700

    Update line.php

--- line.php
+++ line.php
@@ -22,9 +22,9 @@ if ( sizeof($request_array['events']) > 0 )
    {
 		$text = $event['message']['text'];
 		
-		if(($text == "อยากทราบยอดCOVID-19")||($text == "อยากทราบยอดCOVID-19 ครับ")||($text == "COVID-19")){
+		if(($text == "อุณหภูมิตอนนี้")||($text == "อุณหภูมิวันนี้")||($text == "อุณหภูมิ")){
 			$temp = 27;
-			$reply_message = 'ผู่ป่วยสะสม '.$temp.'°C คน
+			$reply_message = 'ขณะนี้อุณหภูมิที่ '.$temp.'°C องศาเซลเซียส';
 		}
 		else if(($text== "ตอนนี้อยู่ที่ไหน")||($text== "ตอนนี้อยู่ไหน")||($text== "อยู่ที่ไหน")||($text== "อยู่ไหน")){
 			$reply_message = 'ขณะนี้อยู่ที่ห้องเรียน IF-5T05 ...!!!';
