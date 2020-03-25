b593a52b9a52e597c5d0c5f816fc7cbcc601e1a3
Jakkrit Tultirat (59160639) <50378545+benzkikyo@users.noreply.github.com>
2020-03-25 10:34:00 +0700

    Update line.php

--- line.php
+++ line.php
@@ -24,9 +24,7 @@ if ( sizeof($request_array['events']) > 0 )
 		
 		if(($text == "ทดสอบ")||($text == "COVID-19")||($text == "อยากทราบยอด COVID-19 ครับ")){
 			$temp = 827;
-			$reply_message = 'ผู้ป่วยสะสม '.$temp.'คน';
-			$temp = 4;
-			$reply_message = 'ผู้เสียชีวิต '.$temp.'คน';
+			$reply_message = 'ผู้ป่วยสะสม ','ผู้เสียชีวิต'.$temp.'คน';
 		}
 		else if(($text== "ตอนนี้อยู่ที่ไหน")||($text== "ตอนนี้อยู่ไหน")||($text== "อยู่ที่ไหน")||($text== "อยู่ไหน")){
 			$reply_message = 'ขณะนี้อยู่ที่ห้องเรียน IF-5T05 ...!!!';
